e1d90b8f7bacf3c446da334b1608860e2b57b928
jérémy <jeremyrenaud@gmail.com>
2016-12-26 17:31:58 +0100

    Ajout page principale

---
+++ WhoAmI/WhoAmI/WhoAmI.UWP/viewmodels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WhoAmI.UWP.viewmodels
+{
+    class MainPageViewModel : ViewModelBase
+    {
+        public ICommand NewCharacterCommand
+        {
+            get; private set;
+        }
+        public ICommand CharactersCommand
+        {
+            get; private set;
+        }
+
+        public MainPageViewModel()
+        {
+            NewCharacterCommand = new RelayCommand(() => ShowNewCharacterView());
+            CharactersCommand = new RelayCommand(() => ShowCharactersView());
+        }
+
+        private void ShowNewCharacterView()
+        {
+
+        }
+
+        private void ShowCharactersView()
+        {
+
+        }
+    }
+}
